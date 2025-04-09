abc7f042256739993d165f95e4e31aa001886bb4
John516csd <2865366928@qq.com>
2025-04-09 21:04:33 +0800

    修改首页

--- src/components/home-hero-banner/index.tsx
+++ src/components/home-hero-banner/index.tsx
@@ -1,11 +1,25 @@
-import { View, Image } from '@tarojs/components'
+import { View, Image, Text } from '@tarojs/components'
 import styles from './index.module.less'
 import EntityLogo from '../../assets/index/entity-logo.png'
 
 const HomeHeroBanner = () => {
     return <View className={styles.wrapper}>
-        <View className={styles.logo_wrapper}>
-            <Image src={EntityLogo} className={styles.logo} />
+        <View className={styles.logo_box}>
+            <View className={styles.logo_wrapper}>
+                <Image src={EntityLogo} className={styles.logo} />
+            </View>
+            {/* <View className={`${styles.emoji_box} ${styles.strong}`}>
+                <Text className={styles.emoji}>💪</Text>
+            </View>
+            <View className={`${styles.emoji_box} ${styles.love}`}>
+                <Text className={styles.emoji}>❤️</Text>
+            </View>
+            <View className={`${styles.emoji_box} ${styles.yoga}`}>
+                <Text className={styles.emoji}>🧘‍♀️</Text>
+            </View>
+            <View className={`${styles.emoji_box} ${styles.run}`}>
+                <Text className={styles.emoji}>🏃</Text>
+            </View> */}
         </View>
     </View>
 }
